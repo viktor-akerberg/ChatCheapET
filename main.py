75dfcc9af6569365c3cfc523b50f379abc5b616d
viktor-akerberg <viktor.akerberg@gmail.com>
2023-03-05 22:25:30 +0100

    Removed tikoken import and lines

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 # Importing required modules
 import os
-
 import openai
-import tiktoken
 
 from chatbot import Chatbot
-
 
 def main():
     # Setting up OpenAI API key from environment variables
     openai.api_key = os.getenv("OPENAI_API_KEY")
-
-    # Getting encoding for GPT-2 model from TikToken library
-    encoder = tiktoken.get_encoding("gpt2")
-
-    # Asserting that encoding and decoding using the encoding results in original string
-    assert encoder.decode(encoder.encode("hello world")) == "hello world"
 
     # Inititate the bot
     Bot = Chatbot(
@@ -25,7 +16,7 @@ def main():
         You are a helpful AI assistant.
         """
     )
-    Bot.gradio_chat()
+    Bot.terminal_chat()
 
 if __name__ == "__main__":
     main()
