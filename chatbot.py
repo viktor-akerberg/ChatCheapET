b6e9f457037d0b2efab91d7f475e0fcc4b02a5d5
viktor-akerberg <viktor.akerberg@gmail.com>
2023-03-05 16:57:21 +0100

    Condensed terminal_chat, added stop reason warning

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,6 @@
 import openai
 import gradio as gr
 
-
 class Chatbot:
     def __init__(
         self,
@@ -21,28 +20,30 @@ class Chatbot:
         # Append the user's prompt as a "user" role message to the list of messages
         self.messages.append({"role": "user", "content": prompt})
         # Get a response from the OpenAI API
-        api_response = openai.ChatCompletion.create(
+        self.api_response = openai.ChatCompletion.create(
             model=self.model,
             max_tokens=self.max_tokens,
             temperature=self.temperature,
             messages=self.messages,
         )
         # Get the content of the response from the API result
-        response = api_response["choices"][0]["message"]["content"]
+        self.latest_response = self.api_response["choices"][0]["message"]["content"]
+        # Print warning if the bot ended its' response due to reaching max token length
+        self.finish_reason = self.api_response["choices"][0]["finish_reason"]
+        if self.finish_reason == "length":
+            print("WARNING, The response stopped because max token length was reached")
         # Append the AI's response as an "assistant" role message to the list of messages
-        self.messages.append({"role": "assistant", "content": response})
-        # Set the latest_response
-        self.latest_response = self.messages[-1]['content']
+        self.messages.append({"role": "assistant", "content": self.latest_response})
         return self.messages
 
     def terminal_chat(self):
-        # Set an initial empty prompt
-        prompt = ""
-        # Loop until the user enters "exit"
-        while prompt != "exit":
-            # Get the user's prompt
+        while True:
+            # Get the users' prompt
             prompt = input("You: ")
-            # Generate the updated list of messages based on the user's prompt
+            # Loop until the user enters "exit"
+            if prompt == "exit":
+                break
+            # Respond and print the response
             self.respond(prompt)
             print(self.latest_response)
 
@@ -52,7 +53,9 @@ class Chatbot:
         self.respond(input)
         messages_tuples_list = []
         for i in range(1, len(self.messages), 2):
-            messages_tuples_list.append((self.messages[i]["content"], self.messages[i + 1]["content"]))
+            messages_tuples_list.append(
+                (self.messages[i]["content"], self.messages[i + 1]["content"])
+            )
         return messages_tuples_list, self.messages
 
     def gradio_chat(self):
